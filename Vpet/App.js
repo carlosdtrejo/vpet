76bf9b6ade66819dc48e015117639a8f7ab13cb6
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2023-03-09 11:48:01 -0600

    rendering gif to screen

--- Vpet/App.js
+++ Vpet/App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const sprite = require("./sprite.gif");
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World of Great Value Digimon!</Text>
+
       <StatusBar style="auto" />
+      <View>
+        <Image
+          source={sprite}
+          style={{
+            width: 100,
+            height: 100,
+            marginTop: 50,
+          }}
+        />
+      </View>
+      {/* <Image
+        src={{ uri: "./sprite.gif" }}
+        style={{ width: 100, height: 100 }}
+        transition={1000}
+        placeholder={blurhash}
+        contentFit="cover"
+      /> */}
     </View>
   );
 }
