a575d0b219310d2bef58fdbae2cf8ec76a746a8a
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2023-03-14 23:11:18 -0500

    adding buttons - bad layout

--- README.md
+++ README.md
@@ -3,21 +3,33 @@
 Requires npm
 Requires Node v14 or higher.
 
-My workaround for Node compatibility issues: 
+My workaround for Node compatibility issues:
 nvm install v18.15.0
 nvm use v18.15.0
 
 Running the app:
-1) cd Vpet
-2) npx expo start
 
-  Android:
-  1) Connect to same wifi
-  2) Install Expo App
-  3) Scan QR Code
+1. cd Vpet
+2. npx expo start
 
-  IOS:
-  1) Scan QR Code
+Android:
 
-  WEB:
-  1) Press 'w'
+1. Connect to same wifi
+2. Install Expo App
+3. Scan QR Code
+
+IOS:
+
+1. Scan QR Code
+
+WEB:
+
+1. Press 'w'
+
+Landscape view is set for Android.
+
+Nice size for landscape:
+height: 250,
+width: 780,
+
+Additional configuration is needed for IOS.
--- Vpet/App.js
+++ Vpet/App.js
@@ -1,34 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+} from "react-native";
+import * as ScreenOrientation from "expo-screen-orientation";
 
-const sprite = require("./animatedSprite4.gif");
+//const sprite = require("./animatedSprite4.gif");
+
+let sprite = require("./eatingSprite2.gif");
+
 const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World of Great Value Digimon!</Text>
-
-      <StatusBar style="auto" />
-      <View>
-        <Image
-          contentFit="cover"
-          source={sprite}
-          style={{
-            height: 100,
-            width: 330,
-            marginTop: 50,
-          }}
-        />
+      <View style={styles.buttonContainer}>
+        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
+        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
+        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
+        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
       </View>
-      {/* <Image
-        src={{ uri: "./sprite.gif" }}
-        style={{ width: 100, height: 100 }}
-        transition={1000}
-        placeholder={blurhash}
-        contentFit="cover"
-      /> */}
+      <ImageBackground source={sprite} style={styles.image}></ImageBackground>
+      <View style={styles.lowerButtonContainer}>
+        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
+        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
+        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
+        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
+      </View>
     </View>
   );
 }
@@ -40,4 +43,35 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  image: {
+    resizeMode: "cover",
+    width: 400,
+    height: 280,
+    flex: 1,
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-evenly",
+    width: 400,
+    paddingTop: 70,
+    marginBottom: -38,
+    zIndex: 1,
+  },
+  lowerButtonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-evenly",
+    width: 400,
+    marginBottom: 50,
+  },
 });
+
+const onPressLearnMore = () => {
+  //For generating alert on buttton click
+  alert("Feed your digimon bitch");
+  ScreenOrientation.getOrientationAsync().then((data) => console.log(data));
+};
