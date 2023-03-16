78ea524dc8030e220203e07dabc83824f2402710
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2023-03-15 22:00:20 -0500

    adding orientation check and landscape view

--- Vpet/App.js
+++ Vpet/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   ImageBackground,
@@ -8,30 +9,143 @@ import {
   Button,
 } from "react-native";
 import * as ScreenOrientation from "expo-screen-orientation";
+import { useEffect, useState } from "react";
 
 //const sprite = require("./animatedSprite4.gif");
 
 let sprite = require("./eatingSprite2.gif");
+let sprite2 = require("./animatedSprite2.gif");
 
 const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function App() {
+  const [orientation, setOrientation] = useState(1);
+
+  useEffect(() => {
+    checkOrientation();
+    const subscription = ScreenOrientation.addOrientationChangeListener(
+      handleOrientationChange
+    );
+    return () => {
+      ScreenOrientation.removeOrientationChangeListeners(subscription);
+    };
+  }, []);
+
+  const checkOrientation = async () => {
+    const orientation = await ScreenOrientation.getOrientationAsync();
+    setOrientation(orientation);
+  };
+
+  const handleOrientationChange = (o) => {
+    setOrientation(o.orientationInfo.orientation);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
-        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
-        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
-        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
-      </View>
-      <ImageBackground source={sprite} style={styles.image}></ImageBackground>
-      <View style={styles.lowerButtonContainer}>
-        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
-        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
-        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
-        <Button onPress={onPressLearnMore} title="Click Me" color="#000000a0" />
-      </View>
+      {orientation === 1 ? (
+        <React.Fragment>
+          <View style={styles.buttonContainer}>
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+          </View>
+          <ImageBackground
+            source={sprite}
+            style={styles.image}
+          ></ImageBackground>
+          <View style={styles.lowerButtonContainer}>
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+          </View>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <View style={styles.buttonContainerLandscape}>
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+          </View>
+          <ImageBackground
+            source={sprite}
+            style={styles.imageLandscape}
+          ></ImageBackground>
+          <View style={styles.lowerButtonContainerLandscape}>
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+            <Button
+              onPress={onPressLearnMore}
+              title="Click Me"
+              color="#000000a0"
+            />
+          </View>
+        </React.Fragment>
+      )}
     </View>
   );
 }
@@ -50,6 +164,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
   },
+  imageLandscape: {
+    resizeMode: "cover",
+    width: 700,
+    height: 330,
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 175,
+    zIndex: -1,
+  },
   buttonContainer: {
     flex: 1,
     flexDirection: "row",
@@ -60,6 +183,14 @@ const styles = StyleSheet.create({
     marginBottom: -38,
     zIndex: 1,
   },
+  buttonContainerLandscape: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-evenly",
+    width: 700,
+    marginBottom: -40,
+  },
   lowerButtonContainer: {
     flex: 1,
     flexDirection: "row",
@@ -67,6 +198,14 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     width: 400,
     marginBottom: 50,
+  },
+  lowerButtonContainerLandscape: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-evenly",
+    width: 700,
+    marginBottom: 25,
   },
 });
 
